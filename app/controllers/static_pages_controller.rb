c6f589c558b71a9c32e12dd5dc2f665f7ea9d778
Simon Budker <sbudker@gmail.com>
2016-10-07 11:22:19 -0700

    Delete signup action, view, and test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,9 +5,6 @@ class StaticPagesController < ApplicationController
   def about
   end
 
-  def signup
-  end
-
   def contact
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,11 +11,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get signup" do
-    get static_pages_signup_url
-    assert_response :success
-  end
-
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
