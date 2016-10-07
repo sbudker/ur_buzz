9c6b0e4971a85b59cfaf954feacded6d53d5ef98
Simon Budker <sbudker@gmail.com>
2016-10-07 11:25:05 -0700

    remove signup route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  get 'static_pages/signup'
-
   get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
