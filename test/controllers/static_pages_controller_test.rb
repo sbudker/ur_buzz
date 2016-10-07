1485f6cdd5c0919afd38052e2933b8c9789335fb
Simon Budker <sbudker@gmail.com>
2016-10-07 11:13:31 -0700

    Add StaticPages controller and home, about, signup, and contact actions

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
+  test "should get signup" do
+    get static_pages_signup_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+
+end
