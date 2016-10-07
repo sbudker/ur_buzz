b6fe29c3e3e7aaf4420be4d0d206a4944620f5a8
Simon Budker <sbudker@gmail.com>
2016-10-07 11:27:43 -0700

    update static page route names

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
 
-  get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
