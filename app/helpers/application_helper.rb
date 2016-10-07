311069de282579e5e0586d70f5a7a7f199fdc4f7
Simon Budker <sbudker@gmail.com>
2016-10-07 11:48:39 -0700

    add basic styling to static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def full_title(page_title = '')
+		base_title = "UR BUZZ"
+		if page_title == ''
+			return base_title
+		else
+			return page_title + " | UR BUZZ"
+		end
+	end
+
 end
