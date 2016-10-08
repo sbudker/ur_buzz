fef63a8e545dae6e17cf88de80a6bc6f1ecbe214
Simon Budker <sbudker@gmail.com>
2016-10-08 07:52:41 -0700

    Add signup form with gravatars and basic styling + basic user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
 
   def new
+  	@user = User.new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    	flash[:success] = "Thanks for signing up!"
+    	redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
