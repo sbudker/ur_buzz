a2c816b5a9b1cb699fb078aa163ef5e016560de8
pferner710 <pferner@optonline.ent>
2016-11-18 15:19:21 -0500

    Added and implemented datepicker gem

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,8 +5,8 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 420 }
   validate :picture_size
-  validates :eventDate, presence: true
-  validates :location, presence: true
+  validates :eventDate, presence: true, allow_blank: false
+  validates :location, presence: true, allow_black: false
 
   private
 
--- db/migrate/20161114032735_add_event_date_to_microposts.rb
+++ db/migrate/20161114032735_add_event_date_to_microposts.rb
@@ -1,5 +1,5 @@
 class AddEventDateToMicroposts < ActiveRecord::Migration[5.0]
   def change
-    add_column :microposts, :eventDate, :string
+    add_column :microposts, :eventDate, :date
   end
 end
