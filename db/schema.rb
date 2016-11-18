978328cfa54f712d0d0553fe90a03b5a11f9b1cf
pferner710 <pferner@optonline.ent>
2016-11-18 18:59:33 -0500

    rollback the db migrate, so eventDate is text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118210033) do
+ActiveRecord::Schema.define(version: 20161118211505) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
