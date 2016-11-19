5d3257d4fc81cc4b16a8e069add2da83a0fc9771
Kevin Gray <kevingray@Kevins-MacBook-Air.local>
2016-11-19 17:50:34 -0500

    fix databse reset problem with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118211505) do
+ActiveRecord::Schema.define(version: 20161119224143) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20161118211505) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "picture"
-    t.text     "eventDate"
+    t.date     "eventDate"
     t.string   "location"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
