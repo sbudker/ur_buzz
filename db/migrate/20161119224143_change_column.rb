dba5412ddb1d215c8fcaa85a231b5d311ebffd83
Simon Budker <sbudker@gmail.com>
2016-11-19 19:34:32 -0800

    Change microposts column evenDate to type :datetime

--- db/migrate/20161119224143_change_column.rb
+++ db/migrate/20161119224143_change_column.rb
@@ -1,4 +1,5 @@
 class ChangeColumn < ActiveRecord::Migration[5.0]
   def change
+    change_column :microposts, :eventDate, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20161119224143) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "picture"
-    t.date     "eventDate"
+    t.datetime "eventDate"
     t.string   "location"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
