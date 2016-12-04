8b6fe04bf9857f3259a1849fd1678aa4d004d542
Kevin Gray <kevingray@Kevins-MacBook-Air.local>
2016-12-04 10:45:43 -0500

    commiting changes to micropost controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
-	before_action :logged_in_user, only: [:create, :destroy]
-	before_action :correct_user,   only: :destroy
+	before_action :logged_in_user, only: [:create, :destroy, :edit]
+	before_action :correct_user,   only: [:destroy, :edit]
 
 	def create
 		@micropost = current_user.microposts.build(micropost_params)
@@ -11,6 +11,10 @@ class MicropostsController < ApplicationController
 			@feed_items = []
 			render 'static_pages/home'
 		end
+	end
+
+	def edit
+		@micropost = Micropost.find(params[:id])
 	end
 
 	def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :microposts, only: [:create, :destroy]
+  resources :microposts, only: [:create, :edit, :destroy]
   resources :relationships,       only: [:create, :destroy]
 
 end
