bb45548432c76b7e8d57879d494fddba81d56b53
Kevin Gray <kevingray@dhcp-10-5-28-141.wireless.rochester.edu>
2016-12-10 22:56:05 -0500

    Added ajax for attending/unattending buttons

--- app/controllers/attends_controller.rb
+++ app/controllers/attends_controller.rb
@@ -1,18 +1,24 @@
 class AttendsController < ApplicationController
 	# User must be logged in to use these methods
-  before_action :logged_in_user
+	before_action :logged_in_user
 
   # Creates an attend relationship between a user and an event
-	def create
-		event = Micropost.find(params[:attending_id])
-    	current_user.attend(event)
-    	redirect_to :back 
-	end
+  def create
+  	@micropost = Micropost.find(params[:attending_id])
+  	current_user.attend(@micropost)
+  	respond_to do |format|
+  		format.html { redirect_to :back }
+  		format.js
+  	end 
+  end
 
 	# Deletes an attend relationship between a user and an event
 	def destroy
-		event = Micropost.find(params[:attending_id])
-    	current_user.unattend(event)
-    	redirect_to :back
+		@micropost = Micropost.find(params[:attending_id])
+		current_user.unattend(@micropost)
+		respond_to do |format|
+			format.html { redirect_to :back }
+			format.js
+		end
 	end
 end
